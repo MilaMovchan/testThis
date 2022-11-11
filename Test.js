2bbe15dfbdc4ef5b504243d89b7d05f3194f976c
Mila <mila18471@gmail.com>
2022-11-11 15:23:41 -0500

    test file this

--- Test.js
+++ Test.js
@@ -2,19 +2,17 @@ function greet(){
     console.log(`Hello ${this.name}, you are ${this.age} years old.`)
 }
 
-
-
 const person1 = {
     name: "Mila",
     age: 34,
     greet: greet
 };
 
-// let person2 = {
-//   name: "Nina",
-//   age: 38
-// //   greet: greet
-
-// }
+const person2 = {
+  name: "Nina",
+  age: 38,
+  greet: greet
+}
 
 console.log(person1.greet());
+console.log(person2.greet());
